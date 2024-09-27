0bc75166fa7813b036b2f0b3010142a9850fc651
Eva Lumean <dkorcagin6@gmail.com>
2024-09-27 15:46:16 +0500

    Added output of the count of occurrences of characters

--- StringProcissing/Program.cs
+++ StringProcissing/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 class Program
 {
@@ -16,9 +17,34 @@ class Program
                 break; // Выходим из цикла, если строка пустая
             }
 
-            string processedString = ProcessString(inputString);
-            Console.WriteLine("Обработанная строка: " + processedString);
+            if (CheckIfValidString(inputString))
+            {
+                string processedString = ProcessString(inputString);
+                Console.WriteLine("Обработанная строка: " + processedString);
+
+                Dictionary<char, int> charCounts = CountCharacterOccurrences(processedString);
+                foreach (var kvp in charCounts)
+                {
+                    Console.WriteLine($"Символ '{kvp.Key}' встречается {kvp.Value} раз.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: Введены неподходящие символы.");
+            }
         }
+    }
+
+    static bool CheckIfValidString(string input)
+    {
+        foreach (char c in input)
+        {
+            if (!char.IsLetter(c) || !char.IsLower(c) || c < 'a' || c > 'z')
+            {
+                return false;
+            }
+        }
+        return true;
     }
 
     static string ProcessString(string input)
@@ -50,5 +76,24 @@ class Program
         char[] charArray = str.ToCharArray();
         Array.Reverse(charArray);
         return new string(charArray);
+    }
+
+    static Dictionary<char, int> CountCharacterOccurrences(string str)
+    {
+        Dictionary<char, int> charCounts = new Dictionary<char, int>();
+
+        foreach (char c in str)
+        {
+            if (charCounts.ContainsKey(c))
+            {
+                charCounts[c]++;
+            }
+            else
+            {
+                charCounts.Add(c, 1);
+            }
+        }
+
+        return charCounts;
     }
 }
