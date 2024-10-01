6c905e7b0d8978548e7534b7a1b73b84f917256e
Eva Lumean <dkorcagin6@gmail.com>
2024-10-01 12:19:40 +0500

    Implemented two character sorting class: Quicksort, Treesort.

--- StringProcissing/Program.cs
+++ StringProcissing/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 class Program
 {
@@ -16,9 +17,67 @@ class Program
                 break; // Выходим из цикла, если строка пустая
             }
 
-            string processedString = ProcessString(inputString);
-            Console.WriteLine("Обработанная строка: " + processedString);
+            if (CheckIfValidString(inputString))
+            {
+                string processedString = ProcessString(inputString);
+                Console.WriteLine("Обработанная строка: " + processedString);
+
+                Dictionary<char, int> charCounts = CountCharacterOccurrences(processedString);
+                foreach (var kvp in charCounts)
+                {
+                    Console.WriteLine($"Символ '{kvp.Key}' встречается {kvp.Value} раз.");
+                }
+
+                string longestVowelSubstring = FindLongestVowelSubstring(processedString);
+                Console.WriteLine($"Самая длинная подстрока, начинающаяся и заканчивающаяся на гласную: {longestVowelSubstring}");
+
+                Console.WriteLine("Выберите алгоритм сортировки:");
+                Console.WriteLine("1. Quicksort");
+                Console.WriteLine("2. Treesort");
+
+                int choice;
+                if (int.TryParse(Console.ReadLine(), out choice))
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            Quicksort quicksort = new Quicksort();
+                            string sortedStringQuicksort = quicksort.Sort(processedString);
+                            Console.WriteLine("Отсортированная обработанная строка (Quicksort): " + sortedStringQuicksort);
+                            break;
+                        case 2:
+                            Treesort treesort = new Treesort();
+                            treesort.BuildTree(processedString);
+                            string sortedStringTreesort = treesort.GetSortedString();
+                            Console.WriteLine("Отсортированная обработанная строка (Treesort): " + sortedStringTreesort);
+                            break;
+                        default:
+                            Console.WriteLine("Некорректный выбор алгоритма.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный ввод.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: Введены неподходящие символы.");
+            }
         }
+    }
+
+    static bool CheckIfValidString(string input)
+    {
+        foreach (char c in input)
+        {
+            if (!char.IsLetter(c) || !char.IsLower(c) || c < 'a' || c > 'z')
+            {
+                return false;
+            }
+        }
+        return true;
     }
 
     static string ProcessString(string input)
@@ -50,5 +109,51 @@ class Program
         char[] charArray = str.ToCharArray();
         Array.Reverse(charArray);
         return new string(charArray);
+    }
+
+    static Dictionary<char, int> CountCharacterOccurrences(string str)
+    {
+        Dictionary<char, int> charCounts = new Dictionary<char, int>();
+
+        foreach (char c in str)
+        {
+            if (charCounts.ContainsKey(c))
+            {
+                charCounts[c]++;
+            }
+            else
+            {
+                charCounts.Add(c, 1);
+            }
+        }
+
+        return charCounts;
+    }
+
+    static string FindLongestVowelSubstring(string str)
+    {
+        string longestSubstring = "";
+        HashSet<char> vowels = new HashSet<char> { 'a', 'e', 'i', 'o', 'u', 'y' };
+
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (vowels.Contains(str[i]))
+            {
+                for (int j = str.Length - 1; j > i; j--)
+                {
+                    if (vowels.Contains(str[j]))
+                    {
+                        string substring = str.Substring(i, j - i + 1);
+                        if (substring.Length > longestSubstring.Length)
+                        {
+                            longestSubstring = substring;
+                        }
+                        break; // Найдена первая гласная с конца
+                    }
+                }
+            }
+        }
+
+        return longestSubstring;
     }
 }
---
+++ StringProcissing/Treesort.cs
@@ -0,0 +1,63 @@
+﻿class Treesort
+{
+    private class TreeNode
+    {
+        public char val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(char c)
+        {
+            val = c;
+            left = null;
+            right = null;
+        }
+    }
+
+    private TreeNode root;
+
+    private TreeNode Insert(TreeNode node, char c)
+    {
+        if (node == null)
+        {
+            node = new TreeNode(c);
+        }
+        else
+        {
+            if (c < node.val)
+            {
+                node.left = Insert(node.left, c);
+            }
+            else
+            {
+                node.right = Insert(node.right, c);
+            }
+        }
+        return node;
+    }
+
+    public void BuildTree(string str)
+    {
+        foreach (char c in str)
+        {
+            root = Insert(root, c);
+        }
+    }
+
+    private void InOrderTraversal(TreeNode node, List<char> sortedList)
+    {
+        if (node != null)
+        {
+            InOrderTraversal(node.left, sortedList);
+            sortedList.Add(node.val);
+            InOrderTraversal(node.right, sortedList);
+        }
+    }
+
+    public string GetSortedString()
+    {
+        List<char> sortedList = new List<char>();
+        InOrderTraversal(root, sortedList);
+        return new string(sortedList.ToArray());
+    }
+}
